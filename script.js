19a7a1b6ac2956486c4c81dd59fd40c7d1f3841a
𝔗𝔞𝔯𝔦𝔮 <quintremington@gmail.com>
2020-08-13 23:29:41 +0200

    Removed background image.

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-
-
 // PAGE LOAD
-window.onload = function() {loadHeader()};
+window.onload = function () {
+    loadHeader()
+    loadPopup()
+};
 
 function loadHeader() {
     $('#header').addClass("show")
 }
 
 // "STICKY" NAVIGATION BAR
-document.addEventListener('DOMContentLoaded', function() {
-  // When the event DOMContentLoaded occurs, it is safe to access the DOM
+document.addEventListener('DOMContentLoaded', function () {
+    // When the event DOMContentLoaded occurs, it is safe to access the DOM
 
-  // When the user scrolls the page, execute myFunction
-  window.addEventListener('scroll', stickyNavigation);
+    // When the user scrolls the page, execute myFunction
+    window.addEventListener('scroll', stickyNavigation);
 
-  // Get the navbar
+    // Get the navbar
     var navbar = document.getElementById("navbar");
     var navbarLinks = document.getElementById("nav-link-div");
 
@@ -29,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
             navbarLinks.classList.remove("stickyNavLinks");
         }
     }
-    });
+});
 
 
 // // TOP SCROLL
@@ -82,7 +83,63 @@ $(function () {
 
 
 function darkMode() {
-    console.log("DARK MODE TOGGLED")
+    console.log("DARK MODE TOGGLED");
     $('.logo').toggleClass('active');
     $('body').toggleClass('dark');
+}
+
+
+// POPUP
+
+function loadPopup() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    var body = document.getElementsByName("body");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("myImg");
+    console.log("PASS");
+    var modalImg = document.getElementById("img01");
+    console.log("PASS");
+    var captionText = document.getElementById("caption");
+    console.log("PASS");
+
+
+    img.onclick = function () {
+        console.log("PASS");
+
+        modal.style.display = "block";
+
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+
+        // When the modal is shown, we want a fixed body
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${window.scrollY}px`;
+
+    };
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+        const scrollY = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    };
+
+    window.onclick = function(event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+        const scrollY = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+      }
+    }
+
 }
