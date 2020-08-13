9ba89dfd60bf5feacde77a48d8a995aa4d52df06
𝔗𝔞𝔯𝔦𝔮 <quintremington@gmail.com>
2020-08-14 00:57:31 +0200

    Removed background image.

--- script.js
+++ script.js
@@ -92,38 +92,52 @@ function darkMode() {
 // POPUP
 
 function loadPopup() {
+
     // Get the modal
     var modal = document.getElementById("myModal");
-    var body = document.getElementsByName("body");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById("myImg");
-    console.log("PASS");
-    var modalImg = document.getElementById("img01");
-    console.log("PASS");
-    var captionText = document.getElementById("caption");
-    console.log("PASS");
+    var imgLandscape = document.getElementById("imgLandscape");
+    var imgPortrait = document.getElementById("imgPortrait");
 
+    var modalImgL = document.getElementById("img01");
+    var modalImgP = document.getElementById("img02");
 
-    img.onclick = function () {
-        console.log("PASS");
+    f();
 
+    function f(){
         modal.style.display = "block";
 
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
+        // if screen bla
+        // modalImg.src = imgLandscape.src;
+
+
+        if (document.documentElement.clientWidth <= 800) {
+            modalImgL.src = imgPortrait.src;
+                    console.log("PORTRAIT");
+        }
+        else{
+            modalImgP.src = imgLandscape.src;
+                    console.log("LANDSCAPE");
+
+        }
+
 
         // When the modal is shown, we want a fixed body
         document.body.style.position = 'fixed';
         document.body.style.top = `-${window.scrollY}px`;
-
-    };
+    }
 
 // Get the <span> element that closes the modal
+        console.log("PASS");
+
     var span = document.getElementsByClassName("close")[0];
+        console.log("PASS");
 
 // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
+        console.log("PASS");
+
         modal.style.display = "none";
         const scrollY = document.body.style.top;
         document.body.style.position = '';
