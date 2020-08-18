83cd7388dd44768604108a161838cfb2b040a9e5
𝔗𝔞𝔯𝔦𝔮 <quintremington@gmail.com>
2020-08-18 18:15:02 +0200

    Removed background image.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
     cookieDarkMode();
 
     loadHeader();
-    cookiePopup();
+    // darkModePopup();
     setDarkModeCookie();
     getDarkModeCookie();
 
@@ -79,23 +79,21 @@ $(function () {
 
 // DARK MODE
 
+// DARK MODE COOKIE
 function cookieDarkMode() {
     var $body = $('body');
 
-    if ($body.hasClass('dark') == false && Cookies.get("dark mode", {domain: 'localhost'}) === 'true') {
+    if ($body.hasClass('dark') == false && Cookies.get("darkMode", {domain: 'localhost'}) === 'true') {
         console.log("DARK MODE = " + $body.hasClass('dark') + ", should be TRUE so TURN DARK MODE ON ");
         $body.toggleClass('dark');
     }
 
-    if ($body.hasClass('dark') == true && Cookies.get("dark mode", {domain: 'localhost'}) === 'false') {
+    if ($body.hasClass('dark') == true && Cookies.get("darkMode", {domain: 'localhost'}) === 'false') {
         console.log("DARK MODE = " + $body.hasClass('dark') + ", should be TRUE so TURN DARK MODE ON ");
         $body.toggleClass('dark');
     }
 }
 
-function darkModeFromLastSession() {
-
-}
 
 function darkMode() {
     // console.log("DARK MODE TOGGLED");
@@ -111,40 +109,195 @@ function setDarkModeCookie() {
 
     if ($body.hasClass('dark') == false) {
         console.log("DARK MODE = " + $body.hasClass('dark'));
-        Cookies.set("dark mode", 'false');
+        Cookies.set("darkMode", 'false');
     }
 
     if ($body.hasClass('dark') == true) {
         console.log("DARK MODE = " + $body.hasClass('dark'));
-        Cookies.set("dark mode", 'true');
+        Cookies.set("darkMode", 'true');
     }
 }
 
 function getDarkModeCookie() {
-    if (Cookies.get("dark mode", {domain: 'localhost'}) === 'false') {
+    if (Cookies.get("darkMode", {domain: 'localhost'}) === 'false') {
         console.log("DARK MODE COOKIE FALSE");
     }
 
-    if (Cookies.get("dark mode", {domain: 'localhost'}) === 'true') {
+    if (Cookies.get("darkMode", {domain: 'localhost'}) === 'true') {
         console.log("DARK MODE COOKIE TRUE");
     }
 
 }
 
 
+// handleModalNotice
 
-// DARK MODE COOKIE
+function handleDarkModeNotice() {
+        // When you close the notice
+    $('.close').click(function(){
+        // Add a cookie so it doesn't show for a year
+        $.cookie('darkModeNotice', 1, {
+            expires: 365,
+            path: '/'
+        });
+    });
+    // Only show if it hasn't been closed before
+    if (!$.cookie('darkModeNotice')) {
+        loadPopup();
+        // When the modal is shown, we want a fixed body
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${window.scrollY}px`;
+    }
+}
 
-// function cookieDarkMode() {
-//     if (Cookies.get("toggle") === true ) {
-//                 console.log("DARK MODE ACTIVE");
-//         darkMode();
-//     }
-//     if (Cookies.get("toggle") === false) {
-//                 console.log("DARK MODE NOT ACTIVE");
-//         darkMode();
-//     }
-// }
+// COOKIE NOTICE
+
+function handleCookiesNotice() {
+    // When you close the notice
+    $('.js_cookieNotice .js_closeButton').click(function(){
+        $(this).closest('.js_cookieNotice').slideUp();
+        const scrollY = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+        // Add a cookie so it doesn't show for a year
+        $.cookie('cookieNotice', 1, {
+            expires: 365,
+            path: '/'
+        });
+    });
+    // Only show if it hasn't been closed before
+    if (!$.cookie('cookieNotice')) {
+        $('.js_cookieNotice').slideDown();
+
+        // When the modal is shown, we want a fixed body
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${window.scrollY}px`;
+    }
+}
+
+$(document).ready(function () {
+  handleCookiesNotice();
+  handleDarkModeNotice();
+});
+
+// DEPENDENCIES
+/*!
+ * jQuery Cookie Plugin v1.4.1
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2006, 2014 Klaus Hartl
+ * Released under the MIT license
+ */
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD (Register as an anonymous module)
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+
+    var pluses = /\+/g;
+
+    function encode(s) {
+        return config.raw ? s : encodeURIComponent(s);
+    }
+
+    function decode(s) {
+        return config.raw ? s : decodeURIComponent(s);
+    }
+
+    function stringifyCookieValue(value) {
+        return encode(config.json ? JSON.stringify(value) : String(value));
+    }
+
+    function parseCookieValue(s) {
+        if (s.indexOf('"') === 0) {
+            // This is a quoted cookie as according to RFC2068, unescape...
+            s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+        }
+
+        try {
+            // Replace server-side written pluses with spaces.
+            // If we can't decode the cookie, ignore it, it's unusable.
+            // If we can't parse the cookie, ignore it, it's unusable.
+            s = decodeURIComponent(s.replace(pluses, ' '));
+            return config.json ? JSON.parse(s) : s;
+        } catch(e) {}
+    }
+
+    function read(s, converter) {
+        var value = config.raw ? s : parseCookieValue(s);
+        return $.isFunction(converter) ? converter(value) : value;
+    }
+
+    var config = $.cookie = function (key, value, options) {
+
+        // Write
+
+        if (arguments.length > 1 && !$.isFunction(value)) {
+            options = $.extend({}, config.defaults, options);
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires, t = options.expires = new Date();
+                t.setMilliseconds(t.getMilliseconds() + days * 864e+5);
+            }
+
+            return (document.cookie = [
+                encode(key), '=', stringifyCookieValue(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path    ? '; path=' + options.path : '',
+                options.domain  ? '; domain=' + options.domain : '',
+                options.secure  ? '; secure' : ''
+            ].join(''));
+        }
+
+        // Read
+
+        var result = key ? undefined : {},
+            // To prevent the for loop in the first place assign an empty array
+            // in case there are no cookies at all. Also prevents odd result when
+            // calling $.cookie().
+            cookies = document.cookie ? document.cookie.split('; ') : [],
+            i = 0,
+            l = cookies.length;
+
+        for (; i < l; i++) {
+            var parts = cookies[i].split('='),
+                name = decode(parts.shift()),
+                cookie = parts.join('=');
+
+            if (key === name) {
+                // If second argument (value) is a function it's a converter...
+                result = read(cookie, value);
+                break;
+            }
+
+            // Prevent storing a cookie that we couldn't decode.
+            if (!key && (cookie = read(cookie)) !== undefined) {
+                result[name] = cookie;
+            }
+        }
+
+        return result;
+    };
+
+    config.defaults = {};
+
+    $.removeCookie = function (key, options) {
+        // Must not alter options, thus extending a fresh object...
+        $.cookie(key, '', $.extend({}, options, { expires: -1 }));
+        return !$.cookie(key);
+    };
+
+}));
 
 
 // POPUP
@@ -241,37 +394,37 @@ function setCookie(name, value) {
         ((secure === true) ? "; secure" : "");
 }
 
-function cookiePopup() {
-    var expdate = new Date();
-    var visit;
-    expdate.setTime(expdate.getTime() + (24 * 60 * 60 * 1000 * 365));
-    if (!(visit = getCookie("visit")))
-        visit = 0;
-    visit++;
-    setCookie("visit", visit, expdate, "/", null, false);
-    var message;
-    if (visit === 0 || visit === 1) {
-        loadPopup();
-    }
-    if (visit > 1) {
-        message = "           I see you came back !";
-    }
-    if (visit === 15) {
-        console.log("RESET");
-        resetCounts();
-    }
-
-    // console.log("\n" + "Your browser has visited this page               \n"
-    //     + "                              " + visit + "\n"
-    //     + "                          time(s)." + "\n" + "\n"
-    //     + message);
-}
+// function darkModePopup() {
+//     var expdate = new Date();
+//     var visit;
+//     expdate.setTime(expdate.getTime() + (24 * 60 * 60 * 1000 * 365));
+//     if (!(visit = getCookie("pageVisits")))
+//         visit = 0;
+//     visit++;
+//     setCookie("pageVisits", visit, expdate, "/", null, false);
+//     var message;
+//     if (visit === 0 || visit === 1) {
+//         loadPopup();
+//     }
+//     if (visit > 1) {
+//         message = "           I see you came back !";
+//     }
+//     if (visit === 15) {
+//         console.log("RESET");
+//         resetCounts();
+//     }
+//
+//     // console.log("\n" + "Your browser has visited this page               \n"
+//     //     + "                              " + visit + "\n"
+//     //     + "                          time(s)." + "\n" + "\n"
+//     //     + message);
+// }
 
 function resetCounts() {
     var expdate = new Date();
     expdate.setTime(expdate.getTime() + (24 * 60 * 60 * 1000 * 365));
     visit = 0;
-    setCookie("visit", visit, expdate, "/", null, false);
+    setCookie("pageVisits", visit, expdate, "/", null, false);
     history.go(1);
 }
 
